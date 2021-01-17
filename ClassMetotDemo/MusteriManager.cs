a07b65a3eb4e874bc5717bfe3f8cf87ceef08f88
Özcan Elmacı <ozcan.elmaci@ogr.deu.edu.tr>
2021-01-18 02:31:45 +0300

    3. ders sonu ödev class metod demo kod

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        ArrayList eklenenMusteriler = new ArrayList();
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Name + " " + musteri.Surname + " has been added as a costumer!!!");
+            eklenenMusteriler.Add(musteri);
+        }
+
+        public void List()
+        {
+            if (eklenenMusteriler.Count > 0)
+            {
+                Console.WriteLine("Müşterilerinizin Listesi");
+                Console.WriteLine("------------------------");
+                foreach (Musteri musteri in eklenenMusteriler)
+                {
+                    Console.WriteLine(musteri.Name + " " + musteri.Surname);
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Müşteri eklemediniz!!!" + "\n");
+            }
+           
+        }
+
+        public void Delete(Musteri musteri)
+        {
+            if (eklenenMusteriler.Contains(musteri))
+            {
+                eklenenMusteriler.Remove(musteri);
+            }
+        }
+
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,61 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MusteriManager musteriIslemleri = new MusteriManager();
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Name = "Özcan";
+            musteri1.Surname = "Elmacı";
+            musteri1.Id = "Id123";
+            musteri1.Age = 20;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Name = "İsim2";
+            musteri2.Surname = "Soyisim2";
+            musteri2.Id = "Id345";
+            musteri2.Age = 22;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Name = "İsim3";
+            musteri3.Surname = "Soyisim3";
+            musteri3.Id = "Id678";
+            musteri3.Age = 23;
+
+            Musteri musteri4 = new Musteri();
+            musteri4.Name = "İsim4";
+            musteri4.Surname = "Soyisim4";
+            musteri4.Id = "Id891";
+            musteri4.Age = 24;
+
+            Musteri musteri5 = new Musteri();
+            musteri5.Name = "İsim5";
+            musteri5.Surname = "Soyisim5";
+            musteri5.Id = "Id011";
+            musteri5.Age = 25;
+
+            Musteri musteri6 = new Musteri();
+            musteri6.Name = "İsim6";
+            musteri6.Surname = "Soyisim6";
+            musteri6.Id = "Id121";
+            musteri6.Age = 26;
+
+            //müşteri ekleme
+            musteriIslemleri.Add(musteri1);
+            musteriIslemleri.Add(musteri2);
+            musteriIslemleri.Add(musteri3);
+            musteriIslemleri.Add(musteri4);
+            musteriIslemleri.Add(musteri5);
+            musteriIslemleri.Add(musteri6);
+            Console.WriteLine();
+
+            //müşterileri listeleme
+            musteriIslemleri.List();
+
+            //bir müşteri sildikten sonra listeleme
+            musteriIslemleri.Delete(musteri3);
+            musteriIslemleri.List();
+
+            Console.ReadLine();
         }
     }
 }
